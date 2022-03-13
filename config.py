fc0500a5083beb8a4bc4834d3ec7b893e3087ace
Erdem Norboev <erdem1600@gmail.com>
2022-03-13 22:54:30 +0300

    af

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "5257423133:AAEnuhj3wwmPv8GwziUx-E7tvPNutFfPYXk"
+
+money = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import telebot
 from extensions import APIException, Convertor
-from config import TOKEN, money
+from config import TOKEN, exchanges
 import traceback
 
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "1596902621:AAEnMFCBOg02PDigS5fAERfgcdKYsuNwb-8"
 
 bot = telebot.TeleBot(TOKEN)
 
-class ConvertionException(Exception):
-    pass
-
-
-@bot.message_handler(content_types=['voice', ])
-def repeat(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'Твой голос божественный')
-pass
-
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
-    text = "Welcome to our world"
+    text = "Приветствие!"
     bot.send_message(message.chat.id, text)
-
-
-@bot.message_handler(content_types=['photo', ])
-def say_lmao(message: telebot.types.Message):
-    bot.reply_to(message, 'Nice meme XDD')
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
-    for i in money.keys():
+    for i in exchanges.keys():
         text = '\n'.join((text, i))
     bot.reply_to(message, text)
 
@@ -38,7 +30,7 @@ def converter(message: telebot.types.Message):
     try:
         if len(values) != 3:
             raise APIException('Неверное количество параметров!')
-
+        
         answer = Convertor.get_price(*values)
     except APIException as e:
         bot.reply_to(message, f"Ошибка в команде:\n{e}" )
@@ -47,6 +39,5 @@ def converter(message: telebot.types.Message):
         bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
     else:
         bot.reply_to(message,answer)
-
 
 bot.polling()
